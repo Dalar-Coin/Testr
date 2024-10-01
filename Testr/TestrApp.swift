3c403e7832387cc6a1c99bba7ad3c5c04a43ccc6
Dennis Alarcon <dennis@Denniss-MacBook-Pro.local>
2024-10-01 12:49:54 -0400

    init commit

---
+++ Testr/TestrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestrApp.swift
+//  Testr
+//
+//  Created by Dennis Alarcon on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TestrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
